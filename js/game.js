a6be1cde791453db5a7c95c8702c861acb91d63e
Kevin Rodrigues <kevrodrigues116@gmail.com>
2015-05-18 14:00:34 +0100

    Drawing actors.

--- js/game.js
+++ js/game.js
@@ -110,5 +110,65 @@ Coin.prototype.type = "coin";
 
 var simpleLevel = new Level(levelPlan);
 
-console.log(simpleLevel.width, "by", simpleLevel.height);
+// console.log(simpleLevel.width, "by", simpleLevel.height);
+
+
+function elt(name, className) {
+	var element = document.createElement(name);
+
+	if (className) {
+		element.className = className;
+	}
+
+	return element;
+}
+
+function DOMDisplay(parent, level) {
+	this.wrap = parent.appendChild(elt("div", "game"));
+	this.level = level;
+
+	this.wrap.appendChild(this.drawBackground());
+	this.actorLayer = null;
+	this.drawFrame();
+}
+
+var scale = 20;
+
+DOMDisplay.prototype.drawBackground = function() {
+  var table = elt("table", "background");
+
+  table.style.width = this.level.width * scale + "px";
+
+  this.level.grid.forEach(function (row) {
+    var rowElt = table.appendChild(elt("tr"));
+    rowElt.style.height = scale + "px";
+    row.forEach(function (type) {
+      rowElt.appendChild(elt("td", type));
+    });
+  });
+  return table;
+};
+
+DOMDisplay.prototype.drawActors = function() {
+	var wrap = elt("div");
+
+	this.level.actors.forEach(function (actor) {
+		var rect = wrap.appendChild(elt("div", "actor " + actor.type));
+
+		rect.style.width = actor.size.x * scale + "px";
+		rect.style.height = actor.size.y * scale + "px";
+		rect.style.left = actor.pos.x * scale + "px";
+		rect.style.top = actor.pos.y * scale + "px";
+	});
+};
+
+
+
+
+
+
+
+
+
+
 
